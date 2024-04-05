46cca2f254d6fcf147db5f410ba1f53333401595
AndreiShishkin <Andreishishkin99@mail.ru>
2024-04-05 09:40:20 +0400

    Изменение значений слайдера

--- js/editPictureModal.js
+++ js/editPictureModal.js
@@ -54,7 +54,7 @@ const editPicture = (range, uploadPicture, sliderContainer, effectLevel) => {
         break;
 
       case 'heat':
-        updateSliderRange(range, 1, 3, 0.1, 3);
+        updateSliderRange(range, 1, 3, 0.1, 2);
         break;
     }
   };
@@ -62,23 +62,23 @@ const editPicture = (range, uploadPicture, sliderContainer, effectLevel) => {
   range.noUiSlider.on('update', (value) => {
     switch (effectValue) {
       case 'chrome':
-        uploadPicture.style.filter = `grayscale(${1})`;
+        uploadPicture.style.filter = `grayscale(${1 - value})`;
         break;
 
       case 'sepia':
-        uploadPicture.style.filter = `sepia(${1})`;
+        uploadPicture.style.filter = `sepia(${1 - value})`;
         break;
 
       case 'marvin':
-        uploadPicture.style.filter = `invert(${100}%)`;
+        uploadPicture.style.filter = `invert(${100 - value}%)`;
         break;
 
       case 'phobos':
-        uploadPicture.style.filter = `blur(${3}px)`;
+        uploadPicture.style.filter = `blur(${3 - value}px)`;
         break;
 
       case 'heat':
-        uploadPicture.style.filter = `brightness(${3})`;
+        uploadPicture.style.filter = `brightness(${3 - value})`;
         break;
     }
 
