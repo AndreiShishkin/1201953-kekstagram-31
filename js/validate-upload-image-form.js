ebfb790d2f3234d53701ca2756c0fd98034e77d4
AndreiShishkin <Andreishishkin99@mail.ru>
2024-04-05 20:45:56 +0400

    Исправление названия регулярного выражения

--- js/validate-upload-image-form.js
+++ js/validate-upload-image-form.js
@@ -1,14 +1,14 @@
 const MAX_COMMENT_LENGTH = 140;
 const MAX_COUNT_HASHTAGS = 5;
 
-const HASHTAGREGEX = /^#[a-zа-яё0-9]{1,19}/i;
+const HASHTAG_REGEX = /^#[a-zа-яё0-9]{1,19}/i;
 
 const validateComment = (value) => value.length <= MAX_COMMENT_LENGTH;
 
 const validateHashtags = (value) => {
   const hashtags = value.toLowerCase().split(' ').filter((element) => element !== '');
   const duplicates = hashtags.filter((element, indexCurrentElement, hashtagsArray) => hashtagsArray.indexOf(element) !== indexCurrentElement);
-  const isValidHashtags = hashtags.every((hashtag) => HASHTAGREGEX.exec(hashtag) ? HASHTAGREGEX.exec(hashtag)[0] === hashtag : false);
+  const isValidHashtags = hashtags.every((hashtag) => HASHTAG_REGEX.exec(hashtag) ? HASHTAG_REGEX.exec(hashtag)[0] === hashtag : false);
   if (duplicates.length === 0 && hashtags.length <= MAX_COUNT_HASHTAGS && isValidHashtags) {
     return true;
   }
@@ -36,7 +36,7 @@ const validateInicial = (validateForm, hashtagsInput, commentInput) => () => {
     }
     const invalidHashtags = [];
     hashtags.forEach((hashtag) => {
-      if (HASHTAGREGEX.exec(hashtag) ? HASHTAGREGEX.exec(hashtag)[0] !== hashtag : true) {
+      if (HASHTAG_REGEX.exec(hashtag) ? HASHTAG_REGEX.exec(hashtag)[0] !== hashtag : true) {
         invalidHashtags.push(hashtag);
       }
     });
